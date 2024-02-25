1b9938784d30163de1fc1fe83de7a205488861e3
hb-nasiruddin <nasiruddin.saiyed@hiddenbrains.in>
2024-02-26 01:36:18 +0530

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,25 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@assets': path.resolve(__dirname, 'src/assets/'),
+        '@components': path.resolve(__dirname, 'src/components/'),
+        '@config': path.resolve(__dirname, 'src/config/'),
+        '@lib': path.resolve(__dirname, 'src/lib/'),
+        '@router': path.resolve(__dirname, 'src/router/'),
+        '@store': path.resolve(__dirname, 'src/store/'),
+        '@styles': path.resolve(__dirname, 'src/styles/'),
+        '@views': path.resolve(__dirname, 'src/views/'),
+        '@atoms': path.resolve(__dirname, 'src/atoms/'),
+        '@molecules': path.resolve(__dirname, 'src/molecules/'),
+        '@organisms': path.resolve(__dirname, 'src/organisms/'),
+        '@templates': path.resolve(__dirname, 'src/templates/'),
+        '@': path.resolve(__dirname, 'src/'),
+      },
+    },
+  },
 });
